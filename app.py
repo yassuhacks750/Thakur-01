9f581ff6a0eeecf3e7e3b677c94663c154a648d1
yassuhacks750 <yassudj8085231272@gmail.com>
2025-07-04 01:50:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-# app.py file ........
-
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello'
-
+@app.route("/")
+def hello():
+    return "Hello, World! from spidy"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
